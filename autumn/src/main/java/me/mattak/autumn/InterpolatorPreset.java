b5a3529283ee981b2cac74884639386004b5f65f
mattak <mattak.me@gmail.com>
2016-01-24 23:18:03 +0900

    feat(InterpolatorPreset): create preset

--- autumn/src/main/java/me/mattak/autumn/InterpolatorPreset.java
+++ autumn/src/main/java/me/mattak/autumn/InterpolatorPreset.java
@@ -1,5 +1,9 @@
 package me.mattak.autumn;
 
+import android.support.v4.view.animation.PathInterpolatorCompat;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 
@@ -8,9 +12,149 @@ import android.view.animation.LinearInterpolator;
  * Created by mattak on 2016/01/24.
  */
 public enum InterpolatorPreset {
+    EaseIn {
+        public Interpolator create(float force) {
+            return new AccelerateInterpolator();
+        }
+    },
+    EaseOut {
+        public Interpolator create(float force) {
+            return new DecelerateInterpolator();
+        }
+    },
+    EaseInOut {
+        public Interpolator create(float force) {
+            return new AccelerateDecelerateInterpolator();
+        }
+    },
     Linear {
         public Interpolator create(float force) {
             return new LinearInterpolator();
+        }
+    },
+    Spring {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.5f, 1.1f + force / 3, 1f, 1f);
+        }
+    },
+    EaseInSine {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.47f, 0f, 0.745f, 0.715f);
+        }
+    },
+    EaseOutSine {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.39f, 0.575f, 0.565f, 1f);
+        }
+    },
+    EaseInOutSine {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.445f, 0.05f, 0.55f, 0.95f);
+        }
+    },
+    EaseInQuad {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.55f, 0.085f, 0.68f, 0.53f);
+        }
+    },
+    EaseOutQuad {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.25f, 0.46f, 0.45f, 0.94f);
+        }
+    },
+    EaseInOutQuad {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.455f, 0.03f, 0.515f, 0.955f);
+        }
+    },
+    EaseInCubic {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.55f, 0.055f, 0.675f, 0.19f);
+        }
+    },
+    EaseOutCubic {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.215f, 0.61f, 0.355f, 1f);
+        }
+    },
+    EaseInOutCubic {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.645f, 0.045f, 0.355f, 1f);
+        }
+    },
+    EaseInQuart {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.895f, 0.03f, 0.685f, 0.22f);
+        }
+    },
+    EaseOutQuart {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.165f, 0.84f, 0.44f, 1f);
+        }
+    },
+    EaseInOutQuart {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.77f, 0.0f, 0.175f, 1f);
+        }
+    },
+    EaseInQuint {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.755f, 0.05f, 0.855f, 0.06f);
+        }
+    },
+    EaseOutQuint {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.23f, 1f, 0.32f, 1f);
+        }
+    },
+    EaseInOutQuint {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.86f, 0f, 0.07f, 1f);
+        }
+    },
+    EaseInExpo {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.95f, 0.05f, 0.795f, 0.035f);
+        }
+    },
+    EaseOutExpo {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.19f, 1f, 0.22f, 1f);
+        }
+    },
+    EaseInOutExpo {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(1f, 0f, 0f, 1f);
+        }
+    },
+    EaseInCirc {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.6f, 0.04f, 0.98f, 0.335f);
+        }
+    },
+    EaseOutCirc {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.075f, 0.82f, 0.165f, 1f);
+        }
+    },
+    EaseInOutCirc {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.785f, 0.135f, 0.15f, 0.86f);
+        }
+    },
+    EaseInBack {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.6f, -0.28f, 0.735f, 0.045f);
+        }
+    },
+    EaseOutBack {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.175f, 0.885f, 0.32f, 1.275f);
+        }
+    },
+    EaseInOutBack {
+        public Interpolator create(float force) {
+            return PathInterpolatorCompat.create(0.68f, -0.55f, 0.265f, 1.55f);
         }
     },;
 
--- example/src/main/java/me/mattak/autumn/example/MainActivity.kt
+++ example/src/main/java/me/mattak/autumn/example/MainActivity.kt
@@ -2,29 +2,52 @@ package me.mattak.autumn.example
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.animation.LinearInterpolator
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Button
 import android.widget.Spinner
 import me.mattak.autumn.AnimatorPreset
+import me.mattak.autumn.InterpolatorPreset
 
 class MainActivity : AppCompatActivity() {
+    val squareView: View by lazy { findViewById(R.id.square) }
+    val animatorSpinner: Spinner by lazy { findViewById(R.id.spinner_animator) as Spinner }
+    val interpolatorSpinner: Spinner by lazy { findViewById(R.id.spinner_interpolator) as Spinner }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val square = findViewById(R.id.square)
-        val spinner = findViewById(R.id.spinner_animator) as Spinner
-        val buttonAnimator = findViewById(R.id.button_animator) as Button
+        animatorSpinner.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, AnimatorPreset.values().map { it.name })
+        interpolatorSpinner.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, InterpolatorPreset.values().map { it.name })
 
-        spinner.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, AnimatorPreset.values().map { it.name })
-
-        buttonAnimator.setOnClickListener {
-            val itemName = spinner.selectedItem.toString()
-            val animatorPreset = AnimatorPreset.valueOf(itemName)
-            val animator = animatorPreset.createAnimator(square, 1.0f, 1000L, 0L, LinearInterpolator()).create()
-            animator.start()
+        squareView.setOnClickListener {
+            animate(squareView, animatorSpinner.selectedItem.toString(), interpolatorSpinner.selectedItem.toString())
         }
+        animatorSpinner.onItemSelectedListener = createOnItemSelectedListener()
+        interpolatorSpinner.onItemSelectedListener = createOnItemSelectedListener()
+    }
+
+    private fun createOnItemSelectedListener(): AdapterView.OnItemSelectedListener {
+        return object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                animate(squareView, animatorSpinner.selectedItem.toString(), interpolatorSpinner.selectedItem.toString())
+            }
+        }
+    }
+
+    private fun animate(square: View, animatorName: String, interpolatorName: String) {
+        val force = 1.0f
+        val duration = 1000L
+        val delay = 0L
+        val animatorPreset = AnimatorPreset.valueOf(animatorName)
+        val interpolatorPreset = InterpolatorPreset.valueOf(interpolatorName)
+        val interpolator = interpolatorPreset.create(force)
+        val animator = animatorPreset.createAnimator(square, force, duration, delay, interpolator).create()
+        animator.start()
     }
 
     class CustomAnimatorListener : Runnable {
