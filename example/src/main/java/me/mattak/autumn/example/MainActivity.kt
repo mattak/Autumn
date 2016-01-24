e4b63b249b782da4ab19fc796bc78cc50948c781
mattak <mattak.me@gmail.com>
2016-01-25 00:09:30 +0900

    feat(example): add seekbar & value label

--- example/src/main/java/me/mattak/autumn/example/MainActivity.kt
+++ example/src/main/java/me/mattak/autumn/example/MainActivity.kt
@@ -3,9 +3,7 @@ package me.mattak.autumn.example
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.*
 import me.mattak.autumn.AnimatorPreset
 import me.mattak.autumn.InterpolatorPreset
 
@@ -13,19 +11,61 @@ class MainActivity : AppCompatActivity() {
     val squareView: View by lazy { findViewById(R.id.square) }
     val animatorSpinner: Spinner by lazy { findViewById(R.id.spinner_animator) as Spinner }
     val interpolatorSpinner: Spinner by lazy { findViewById(R.id.spinner_interpolator) as Spinner }
+    val forceSeekBar: SeekBar by lazy { findViewById(R.id.seekbar_force) as SeekBar }
+    val durationSeekBar: SeekBar by lazy { findViewById(R.id.seekbar_duration) as SeekBar }
+    val delaySeekBar: SeekBar by lazy { findViewById(R.id.seekbar_delay) as SeekBar }
+    val forceTextView: TextView by lazy { findViewById(R.id.value_force) as TextView }
+    val durationTextView: TextView by lazy { findViewById(R.id.value_duration) as TextView }
+    val delayTextView: TextView by lazy { findViewById(R.id.value_delay) as TextView }
+    var force: Float = 1.0f
+    var duration: Long = 1000L
+    var delay: Long = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // TODO: refactoring
         animatorSpinner.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, AnimatorPreset.values().map { it.name })
         interpolatorSpinner.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, InterpolatorPreset.values().map { it.name })
 
-        squareView.setOnClickListener {
-            animate(squareView, animatorSpinner.selectedItem.toString(), interpolatorSpinner.selectedItem.toString())
-        }
+        squareView.setOnClickListener { animate() }
         animatorSpinner.onItemSelectedListener = createOnItemSelectedListener()
         interpolatorSpinner.onItemSelectedListener = createOnItemSelectedListener()
+        val forceAdjust: (SeekBar) -> Unit = {
+            force = it.progress.toFloat() / 100.0f
+            forceTextView.text = force.toString()
+        }
+        val durationAdjust: (SeekBar) -> Unit = {
+            duration = it.progress.toLong()
+            durationTextView.text = duration.toString()
+        }
+        val delayAdjust: (SeekBar) -> Unit = {
+            delay = it.progress.toLong()
+            delayTextView.text = delay.toString()
+        }
+        forceSeekBar.setOnSeekBarChangeListener(this.createOnSeekBarChangeListener(forceAdjust))
+        durationSeekBar.setOnSeekBarChangeListener(this.createOnSeekBarChangeListener(durationAdjust))
+        delaySeekBar.setOnSeekBarChangeListener(this.createOnSeekBarChangeListener(delayAdjust))
+        forceAdjust(forceSeekBar)
+        durationAdjust(durationSeekBar)
+        delayAdjust(delaySeekBar)
+    }
+
+    private fun createOnSeekBarChangeListener(runner: (SeekBar) -> Unit): SeekBar.OnSeekBarChangeListener {
+        return object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                seekBar?.let { runner(it) }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                seekBar?.let { runner(it) }
+                animate()
+            }
+        }
     }
 
     private fun createOnItemSelectedListener(): AdapterView.OnItemSelectedListener {
@@ -34,19 +74,18 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                animate(squareView, animatorSpinner.selectedItem.toString(), interpolatorSpinner.selectedItem.toString())
+                animate()
             }
         }
     }
 
-    private fun animate(square: View, animatorName: String, interpolatorName: String) {
-        val force = 1.0f
-        val duration = 1000L
-        val delay = 0L
+    private fun animate() {
+        val animatorName: String = animatorSpinner.selectedItem.toString()
+        val interpolatorName: String = interpolatorSpinner.selectedItem.toString()
         val animatorPreset = AnimatorPreset.valueOf(animatorName)
         val interpolatorPreset = InterpolatorPreset.valueOf(interpolatorName)
         val interpolator = interpolatorPreset.create(force)
-        val animator = animatorPreset.createAnimator(square, force, duration, delay, interpolator).create()
+        val animator = animatorPreset.createAnimator(squareView, force, duration, delay, interpolator).create()
         animator.start()
     }
 
